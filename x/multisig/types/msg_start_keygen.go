06e81f7c3251da1e819657614232fdd3c5e89146
Vu Viet Tai <vuviettai@gmail.com>
2024-08-01 15:17:26 +0700

    Fix bug cast to LegacyMsg when generate key

--- x/multisig/types/msg_start_keygen.go
+++ x/multisig/types/msg_start_keygen.go
@@ -1,13 +1,18 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 
 	"github.com/axelarnetwork/axelar-core/x/multisig/exported"
 )
 
 var _ sdk.Msg = &StartKeygenRequest{}
+
+// 20240801 Taivv: For amino support.
+var _ legacytx.LegacyMsg = &StartKeygenRequest{}
 
 // NewStartKeygenRequest constructor for StartKeygenRequest
 func NewStartKeygenRequest(sender sdk.AccAddress, keyID exported.KeyID) *StartKeygenRequest {
@@ -34,3 +39,25 @@ func (m StartKeygenRequest) ValidateBasic() error {
 func (m StartKeygenRequest) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{m.Sender}
 }
+
+/*
+* 20240801: Scalar
+* Implement the LegacyMsg interface
+ */
+
+// GetSignBytes returns the bytes all expected signers must sign over for a
+// StartKeygenRequest.
+func (msg StartKeygenRequest) GetSignBytes() []byte {
+	var amino = codec.NewLegacyAmino()
+	return sdk.MustSortJSON(amino.MustMarshalJSON(&msg))
+}
+
+func (msg StartKeygenRequest) Route() string {
+	return "keygenStart"
+}
+
+func (msg StartKeygenRequest) Type() string {
+	return "StartKeygenRequest"
+}
+
+/* End of LegacyMsg interface */
