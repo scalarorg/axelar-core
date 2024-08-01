cabfd05ceadfdd700b662844877e3e97741714da
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-08-01 11:02:52 +0700

    feat(upd): compatible with btc-vault

--- vald/btc/decoders.go
+++ vald/btc/decoders.go
@@ -1,14 +1,19 @@
 package btc
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/axelarnetwork/axelar-core/vald/btc/rpc"
 	"github.com/axelarnetwork/axelar-core/x/evm/types"
 	nexus "github.com/axelarnetwork/axelar-core/x/nexus/exported"
+
+	"github.com/scalarorg/btc-vault/btcvault"
 )
 
 func DecodeEventContractCall(tx *rpc.BTCTransaction) (types.EventContractCall, error) {
+	fmt.Println(btcvault.TagLen)
 	// TODO_SCALAR: Parse the tx data to extract the sender, destination chain, and contract address
 	sender := types.Address(common.BytesToAddress([]byte(tx.Data.Hash)))
 	destinationChain := nexus.ChainName("ethereum-sepolia")
